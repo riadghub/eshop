37917f364aca308237478e7e38102824615a8c7d
Riad <119328612+riadghub@users.noreply.github.com>
2023-05-30 16:37:40 +0200

    Update init.php

--- include/init.php
+++ include/init.php
@@ -8,6 +8,6 @@ $erreur = ''; // Variable qui contiendra les messages d'erreur
 $validation = ''; 
 
 // define("URL","http://localhost/konexio/php/11-Projet/"); // Constante qui contient l'URL du site
-define("URL","https://myfewshop.herokuapp.com/";
+define("URL","https://myfewshop.herokuapp.com/");
 
 //define("RACINE_SITE",$_SERVER['DOCUMENT_ROOT'] ."/konexio/php/11-Projet/"); // Constante qui contient le chemin du site
