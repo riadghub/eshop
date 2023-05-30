7d83a4bdb226c238cdee9a5699a47c7645428276
Riad <119328612+riadghub@users.noreply.github.com>
2023-05-30 16:32:12 +0200

    Update database.php

--- include/database.php
+++ include/database.php
@@ -1,7 +1,11 @@
 <?php
 
 // Connexion à la base de données
-try{
+$db_host = "eu-cdbr-west-03.cleardb.net";
+$db_user = "bd8300fbdc9d13";
+$db_pass = "212166c7";
+$db_name = "heroku_5f3275f3bc4a15a";
+/* try{
     $pdo = new PDO('mysql:host=localhost;dbname=boutique_konexio','root','root',
                     array(PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING, 
                     PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
@@ -10,3 +14,5 @@ try{
     echo 'N° : ' . $e->getCode() . '<br>';
     die('Connexion au serveur impossible.');
 }
+*/
+$connect = mysqli_connect($db_host, $db_user, $db_pass, $db_name) or die("Database Connection Error");
